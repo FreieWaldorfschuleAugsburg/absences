1677003181e0ac81e7abda6f45b0c4a8a0e9fd7b
Linus Groschke <mail@linusgke.de>
2026-01-01 16:07:17 +0100

    Fix wrong error messages

--- app/Helpers/oauth_helper.php
+++ app/Helpers/oauth_helper.php
@@ -38,7 +38,7 @@ function login(): RedirectResponse
 
         return redirect('/');
     } catch (OpenIDConnectClientException $e) {
-        throw new OAuthException('oidc_login_error', $e);
+        throw new OAuthException('login', $e);
     }
 }
 
@@ -55,7 +55,7 @@ function logout(): RedirectResponse
 
         $oidc->signOut($user->getIdToken(), null);
     } catch (OpenIDConnectClientException $e) {
-        throw new OAuthException('oidc_logout_error', $e);
+        throw new OAuthException('logout', $e);
     }
 
     return redirect('/');
@@ -83,7 +83,7 @@ function user(): ?UserModel
 
         return $user;
     } catch (Exception $e) {
-        throw new OAuthException('oidc_refresh_error', $e);
+        throw new OAuthException('refresh', $e);
     }
 }
 
